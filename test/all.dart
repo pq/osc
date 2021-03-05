af4a9f1feed6657aac3b320f01e573ca01e32b90
pq <pquitslund@gmail.com>
2021-03-05 13:28:31 -0800

    - test all

--- test/all.dart
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021, Google LLC. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-import 'convert_test.dart' as convert_test;
-
-void main() {
-  convert_test.main();
-}
